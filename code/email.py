3771740195d7497a338d3bb8ee859e4796f85289
wdy0401 <wdy0401@gmail.com>
2017-08-05 14:18:27 +0800

    163邮箱有发送时被识别为垃圾邮件的问题 改用qq邮箱

--- code/email.py
+++ code/email.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug  3 10:12:39 2017
+Created on Thu Aug  3 13:48:59 2017
 
-@author: admin
+@author: ccc
 """
+
 from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart  
-
 import smtplib
 
-from_addr="wdy0401@163.com"
+from_addr="wdy0401@qq.com"
 to_addr="wdy0401@163.com"
-password=input('Password')
-smtp_server = 'smtp.163.com'
+password='xxxxxxxxx'#这里填写qq邮箱的授权码
+smtp_server='smtp.qq.com'
 
 
 def png(fn,fns):
@@ -27,15 +27,16 @@ def png(fn,fns):
         mime.set_payload(f.read())
         encoders.encode_base64(mime)
         return mime
+
 msg = MIMEMultipart()
-msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
-
-
-msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+msg['Subject'] = Header('Code from zrhx', 'utf-8').encode()
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg.attach(MIMEText('png file', 'plain', 'utf-8'))
 msg.attach(png('../fig/sum.png',"sum.png"))
 msg.attach(png('../fig/part.png',"part.png"))
-
-server = smtplib.SMTP(smtp_server, 25)
+server = smtplib.SMTP_SSL(smtp_server, 465)
 server.set_debuglevel(1)
 server.login(from_addr, password)
-server.sendmail(from_addr, [to_addr], msg.as_string())  
+server.sendmail(from_addr, to_addr, msg.as_string())  
+
